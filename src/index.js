009cf458bc9f36bfe62cfbed993a13335c6753e3
Olha Hrymchak <forwork1177@gmail.com>
2020-10-14 22:36:07 +0300

    added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+
 
 
 ReactDOM.render(
-  <Provider store={null}>
+  <Provider store={store}>
      <BrowserRouter>
-  <React.StrictMode>
+  {/* <React.StrictMode> */}
     <App />
-  </React.StrictMode>
+  {/* </React.StrictMode> */}
   </BrowserRouter>
   </Provider>,
   document.getElementById('root')
